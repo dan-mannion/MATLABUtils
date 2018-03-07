176fb7083dc6d15196e0b441773d0a0ed3465951
dan-mannion <danjohnmannion@gmail.com>
2018-03-07 10:06:59 +0000

    Improves help comments

--- maxk.m
+++ maxk.m
@@ -1,5 +1,5 @@
 function [b,i] = maxk(a, k)
-%MAXK (a, k) Returns the k largest values from a. Returns values(b) and
+%[b, i ] = MAXK (a, k) Returns the k largest values from a. Returns values(b) and
 %indices (i)
 
 [b,i] = sort(a,'descend');
