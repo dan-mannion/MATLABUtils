b78208be6b43a879febe18577288139aa05f770f
dan-mannion <danjohnmannion@gmail.com>
2019-03-15 11:39:15 +0000

    Completes autosave function

--- autoSave.m
+++ autoSave.m
@@ -1,8 +1,20 @@
-%Auto save
-data = [];
-fdir = 'Test';
+function autoSave(fdir, name)
+%%AUTOSAVE (fdir, data) Saves data with an auto increment
+% to the directory fdir. Data should be a char array with the name of
+% variable
+
+tmp = name;
+eval([name, '=evalin(''base'',name)']);
+
 if(~exist(fdir,'dir'))
    mkdir(fdir); 
 end
 
-fname = sprintf('%s\\%04d', fdir, 0);
+id = 0;
+fname = sprintf('%s\\%04d.mat', fdir, id);
+while(exist(fname, 'file'))
+id = id+1;
+fname = sprintf('%s\\%04d.mat', fdir, id);
+end
+save(fname, name);
+end
